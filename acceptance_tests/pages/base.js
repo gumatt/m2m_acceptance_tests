072ab1ae4516fdd0ef5b66490dc23c639181439d
gumatt <gumatt@gmail.com>
2017-08-17 13:32:18 -0700

    refactors BaseValidator and BasePage

---
+++ acceptance_tests/pages/base.js
@@ -0,0 +1,38 @@
+import { t } from 'testcafe';
+import { getLocation } from '../helpers/common'
+
+class BaseValidator {
+  constructor(map) {
+    this.t = t;
+    this.m = map;
+  }
+
+  async elementStyle(el, attrib, value) {
+    await this.t.expect(el.getStyleProperty(attrib)).eql(value);
+  }
+
+  async location(url) {
+    await this.t.expect(getLocation()).eql(url);
+  }
+}
+
+class BasePage {
+  constructor(url, map, validator) {
+    this.t = t;
+    this.v = validator;
+    this.m = map;
+    this.url = url;
+  }
+
+  async navigateToAs(user) {
+    await this.t
+      .useRole(user)
+      .navigateTo(this.url);
+  }
+
+  validate() {
+    return this.v;
+  }
+}
+
+export { BaseValidator, BasePage };
